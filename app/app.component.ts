c89a8cf56f4557a37a06db057f6eaf260e4948ed
etgramli <etgramlich@gmx.de>
2016-12-14 14:57:34 +0100

    New component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,3 +5,11 @@ import { Component } from '@angular/core';
   template: `<h1>Hello {{name}}</h1>`,
 })
 export class AppComponent  { name = 'Angular'; }
+
+
+@Component({
+	selector: 'moeraki-kemu',
+	template `<h1>Greetings from Moeraki Kemu</h1>
+	<p>1 + 2 = {{1+2}}</p>`,
+})
+export class AppMK{}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { AppMK }         from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppMK ],
+  bootstrap:    [ AppMK ]
 })
 export class AppModule { }
